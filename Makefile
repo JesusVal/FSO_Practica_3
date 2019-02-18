0cc01185d83dfda7c85ff4fe08d858a403428f61
JesusVal <is703557@iteso.mx>
2019-02-18 10:49:59 -0600

    Se agrego un makefile para leibniz_1 y se agregaron ciclos a leibniz_1

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+leibniz_1: leibniz_1.c 
+	gcc -o leibniz_1 leibniz_1.c
+	
--- leibniz_1.c
+++ leibniz_1.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define CICLOS 1000000000
+
 int main()
 {
 /*Variables para contar tiempo*/
@@ -12,12 +14,18 @@ int main()
 	long lElapsedTime;
 	struct timeval ts;
 	
+/*Variables de programa*/
+	int i;
+	
 /*Tiempo inicial: Al inicio de la ejecucion*/
 	gettimeofday(&ts, NULL);
 	start_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo inicial
 	
 	
-	
+	for(int i = 0 ; i < CICLOS ; i++)
+	{
+		
+	}
 	printf("Soy una ejecucion\n");
 	
 	
@@ -27,6 +35,7 @@ int main()
 	
 	elapsed_time = (int) (stop_ts - start_ts);
 	printf("proceso %d, %d microsegundos\n", getpid(), elapsed_time);
+	printf("Segundos: %f\n", (double) elapsed_time/1000000.0);
 
 	
 	return 0;
