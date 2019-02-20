9c62d9653f5e5933e84389793384229397856390
JesusVal <is703557@iteso.mx>
2019-02-20 14:16:36 -0600

    Se creo el archivo del caso 3 y modifico el makefile

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ leibniz_1: leibniz_1.c
 leibniz_2: leibniz_2.c
 	gcc -o leibniz_2 leibniz_2.c -lm -lpthread
 	
+leibniz_3: leibniz_3.c
+	gcc -o leibniz_3 leibniz_3.c -lm
+	
---
+++ leibniz_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <pthread.h>
+
+void *holamundo(void *args)
+{
+	sleep(2);
+	printf("Hola mundo\n");
+}
+
+int main()
+{
+	/*
+	pthread_t tid;
+	
+	pthread_create(&tid,NULL,holamundo,NULL);
+	
+	pthread_join(tid,NULL);
+	*/
+	printf("Hola mundo\n");
+
+	return 0;
+}
