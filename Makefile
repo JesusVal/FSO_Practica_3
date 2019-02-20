ad51ed4564502de21ac222f380189ec2e645bb8a
JesusVal <is703557@iteso.mx>
2019-02-20 13:55:45 -0600

    Caso 2 con hilos posix completado y makefile adaptado

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ leibniz_1: leibniz_1.c
 	gcc -o leibniz_1 leibniz_1.c -lm
 
 leibniz_2: leibniz_2.c
-	gcc -o leibniz_2 leibniz_2.c -lm
+	gcc -o leibniz_2 leibniz_2.c -lm -lpthread
 	
--- leibniz_2.c
+++ leibniz_2.c
@@ -3,47 +3,79 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <pthread.h>
 
 #define CICLOS 2000000000
-//#define CICLOS 10000
+#define NTHREADS 4
+
+
+double valueArr[NTHREADS];
+
+
+void *leibniz(void *args);
+
 
 int main()
 {
-/*Variables para contar tiempo*/
+	int i;
+	double value = 0.0;
+	pthread_t tid[NTHREADS];
+	int parArr[NTHREADS];
+
 	long long start_ts;
 	long long stop_ts;
-	int elapsed_time;
+	long long elapsed_time;
 	long lElapsedTime;
 	struct timeval ts;
-	
-/*Variables de programa*/
-	int i;
-	double value = 0;
-	
-/*Tiempo inicial: Al inicio de la ejecucion*/
+
 	gettimeofday(&ts, NULL);
-	start_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo inicial
+	start_ts = ts.tv_sec; // Tiempo inicial
 	
-/*
-	for(int i = 0 ; i < CICLOS ; i++)
-	{
-		value = value + pow(-1, i)/(2.0*i+1);
-		//printf("pow %f\n",pow(-1,i));
-	}
-	value = value * 4;
-	*/
-	//printf("Soy una ejecucion %f\n", value);
-	
-	
-/*Tiempo final: Al final de la ejecucion*/
-	gettimeofday(&ts, NULL);
-	stop_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo final
-	
-	elapsed_time = (int) (stop_ts - start_ts);
-	printf("proceso %d, %d microsegundos\n", getpid(), elapsed_time);
-	printf("Segundos: %f\n", (double) elapsed_time/1000000.0);
-	printf("Valor aproximado de pi : %f\n", value);
 
 	
-	return 0;
+	
+	for(i=0;i<NTHREADS;i++)
+	{
+		parArr[i] = i;
+		pthread_create(&tid[i],NULL,leibniz,(void *) &parArr[i]);
+	}
+	
+
+	for(i=0;i<NTHREADS;i++)
+		pthread_join(tid[i],NULL);
+
+	for(i = 0 ; i < NTHREADS ; i++)
+		value = value + valueArr[i];
+
+	value = value * 4;
+	
+	gettimeofday(&ts, NULL);
+	stop_ts = ts.tv_sec; // Tiempo final
+
+	elapsed_time = stop_ts - start_ts;
+	printf("------------------------------\n");
+	printf("Aproximacion: %e\n", value);
+	printf("TIEMPO TOTAL, %d segundos\n", (int) elapsed_time);
+}
+
+
+void *leibniz(void *args)
+{
+	int nthread = *((int *) args);
+
+	valueArr[nthread] = 0.0;
+	int i;
+	int inicio = nthread * (CICLOS/NTHREADS);
+	int final = (nthread+1) * (CICLOS/NTHREADS);
+
+	//double valuetemp = 0;
+
+	for(int i =  inicio ; i < final ; i++)
+	{
+		valueArr[nthread] = valueArr[nthread] + pow(-1, i)/(2.0*i+1);
+		//printf("pow %f\n",pow(-1,i));
+	}
+
+
+	printf("Hola mundo desde el hilo %d\n",nthread);
 }
